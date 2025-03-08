d6aa32444743f05dd0e5c5172835946e2ff99e63
arya adi <wayanurip733@gmail.com>
2025-03-08 12:19:27 +0800

    upload projek 1

---
+++ pertemuan1/src/pertemuan1/Pertemuan1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pertemuan1;
+
+/**
+ *
+ * @author arya adi natha
+ * TGL: 8 Maret 2025
+ */
+public class Pertemuan1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("latihan bahasa java");
+    }
+    
+}
